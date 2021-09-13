0f60e5aae353d114ebfa998a4541644b39d795fe
izabellalarsson <izzwii.izabella@gmail.com>
2021-09-13 20:50:45 +0200

    remove log for error

--- server/database.js
+++ server/database.js
@@ -4,5 +4,5 @@ module.exports = async () => {
   await mongoose
     .connect('mongodb://localhost:27017/mytodos')
     .then(() => console.log('DB Connected 🎉'))
-    .catch((error) => console.log('Error in DB Connection 😔', error));
+    .catch((error) => console.log('Error in DB Connection 😔'));
 };
